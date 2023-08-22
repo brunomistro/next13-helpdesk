dff92a8408c30f7fef84f3e83717ae69d7933b94
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2023-08-21 23:00:52 -0300

    fetch details

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,30 +1,34 @@
-async function getTickets() {
-	const res = await fetch("http://localhost:4000/tickets", {
-		next: {
-			revalidate: 0 //never use data from cache
-		}
-	})
+import Link from "next/link";
 
-	return res.json()
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0, //never use data from cache
+    },
+  });
+
+  return res.json();
 }
 
 export default async function TicketList() {
-	const tickets = await getTickets()
+  const tickets = await getTickets();
 
-	return (
-		<>
-		{tickets.map((ticket) => (
-			<div key={ticket.id} className="card my-5">
-				<h3>{ticket.title}</h3>
-				<p>{ticket.body.slice(0, 200)}...</p>
-				<div className={`pill ${ticket.priority}`}>
-					{ticket.priority} priority
-				</div>
-			</div>
-		))}
-		{tickets.length === 0 && (
-			<p className="text-center">There are no open tickets, yay!</p>
-		)}
-		</>
-	)
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  );
 }
