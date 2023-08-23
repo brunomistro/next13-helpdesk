81416c97f1b78d63698edc10e5c8f8d02007ec9c
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2023-08-23 10:17:21 -0300

    loading page

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,15 +1,22 @@
+import { Suspense } from "react";
 import TicketsList from "./TicketList";
+import Loading from "../loading";
 
 export default function Tickets() {
-	return (
-		<main>
-			<nav>
-				<div>
-					<h2>Tickets</h2>
-					<p><small>Currently open tickets.</small></p>
-				</div>
-			</nav>
-			<TicketsList />
-		</main>
-	)
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+
+      <Suspense fallback={<Loading />}>
+        <TicketsList />
+      </Suspense>
+    </main>
+  );
 }
